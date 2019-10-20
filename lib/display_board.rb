927fc2a736f22e3d5c5de51b8d557fa8af858269
Stela Capsa <stela.capsa@gmail.com>
2019-10-20 20:50:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,25 +1,13 @@
 def display_board(board)
-#print an empty board
+#print an empty board 1
  board = [" ", "  ", " ", " ", " ", " ", " ", " ", " "]
-
- row_1 = ("   |   |   ")
- row_2 = ("-----------")
- row_3 = ("   |   |   ")
- row_4 = ("-----------")
- row_5 = ("   |   |   ")
-
-   end
-
-
-#print a board with the x in the center
-  def display_board(board)
-board = [" ", "  ", " ", "x ", " ", " ", " ", " ", " "]
-
-row_1 = ("   |   |   ")
-row_2 = ("-----------")
-row_3 = ("   |   |   ")
-row_4 = ("-----------")
-row_5 = ("   |   |   ")
-puts "#{display_board[3]}"
+row = "-------------"
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts row
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts row
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 end
+
+
