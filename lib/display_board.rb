2a3c538cce6a00140c92bfee499b1f00941f1759
Stela Capsa <stela.capsa@gmail.com>
2019-10-20 19:23:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(the_board)
+
+ the_board = [" ", "  ", " ", " ", " ", " ", " ", " ", " "]
+puts "Where would you like to place x?"
+puts = "#{the_board[0]}   |#{the_board[1]}   |#{the_board[2]}   "
+puts = "-----------"
+puts = "#{the_board[3]}   |#{the_board[4]}   |#{the_board[5]}   "
+puts = "-----------"
+puts = "#{the_board[6]}   |#{the_board[7]}   |#{the_board[8]}  "
+
+  end
+
+
+the_board = [" ", "  ", " ", "x ", " ", " ", " ", " ", " "
